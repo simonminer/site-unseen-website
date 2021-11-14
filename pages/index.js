75431d5d80bda4932361d08c14e0466f317ab010
Simon Miner <simon.miner@gmail.com>
2021-11-14 11:23:35 -0500

    Added more favicon files and configuration (per https://favicon.io/favicon-converter/)

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,10 @@ export default function Home() {
       <Head>
         <title>Site Unseen</title>
         <meta name="description" content="Site Unseen: A JavaScript library, browser extension, and web app to build empathy for blind web users" />
-        <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+        <link rel="manifest" href="/site.webmanifest" />
       </Head>
 
       <main className={styles.main}>
